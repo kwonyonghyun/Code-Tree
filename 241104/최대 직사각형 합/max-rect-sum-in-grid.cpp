d8151842e855e852d7198efcc01b31fc96a14865
kwonyonghyun <106018526+kwonyonghyun@users.noreply.github.com>
2024-11-04 08:59:50 +0900

    Title: 최대 직사각형 합, Time: 1019ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 241104/최대 직사각형 합/max-rect-sum-in-grid.cpp
+++ 241104/최대 직사각형 합/max-rect-sum-in-grid.cpp
@@ -4,31 +4,40 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     int n;
-    cin>>n;
-    vector<vector<int>> board(n+1,vector<int>(n+1));
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cin>>board[i][j];
+    cin >> n;
+    
+    vector<vector<int>> board(n+1, vector<int>(n+1, 0));
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            cin >> board[i][j];
         }
     }
-    vector<vector<int>> prefixSum(n+1,vector<int>(n+1));
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            prefixSum[i][j]=prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + board[i][j];
+    vector<vector<int>> prefixSum(n+1, vector<int>(n+1, 0));
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            prefixSum[i][j] = prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1] + board[i][j];
         }
     }
-    int result=INT_MIN;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            for(int x=i;x<=n;x++){
-                for(int y=j;y<=n;y++){
-                    result=max(result, prefixSum[x][y]-prefixSum[x-i][y]-prefixSum[x][y-j]+prefixSum[x-i][y-j]);
+    
+    int result = INT_MIN;
+    for(int i1 = 1; i1 <= n; i1++) {
+        for(int j1 = 1; j1 <= n; j1++) {
+            for(int i2 = i1; i2 <= n; i2++) {
+                for(int j2 = j1; j2 <= n; j2++) {
+                    int sum = prefixSum[i2][j2] 
+                           - prefixSum[i1-1][j2] 
+                           - prefixSum[i2][j1-1] 
+                           + prefixSum[i1-1][j1-1];
+                    result = max(result, sum);
                 }
             }
         }
     }
-    cout<<result;
+    
+    cout << result;
     return 0;
 }
-//-1000 1000 이하의 숫자로만 이루어진 n*n 크기의 2차원 격
