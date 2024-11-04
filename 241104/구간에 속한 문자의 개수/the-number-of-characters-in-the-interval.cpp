1533637339ecbd4304a5f29c0c4adc43a40d2316
kwonyonghyun <106018526+kwonyonghyun@users.noreply.github.com>
2024-11-04 09:16:09 +0900

    Title: 구간에 속한 문자의 개수, Time: 91ms, Memory: 11MB, Status: Passed - CodeTree

--- 241104/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.cpp
+++ 241104/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <string>
+#define MAX_N 1000
 
 using namespace std;
 
+int n, m, k;
+int board[MAX_N + 1][MAX_N + 1][3];
+int prefixSum[MAX_N + 1][MAX_N + 1][3];
+
 int main() {
-    int n, m, k;
-    cin >> n >> m >> k;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     
-    vector<vector<vector<int>>> board(n+1, vector<vector<int>>(m+1, vector<int>(3, 0)));
+    cin >> n >> m >> k;
     
     for(int i = 1; i <= n; i++) {
         string input;
         cin >> input;
-        for(int j = 0; j < m; j++) { 
-            board[i][j+1][input[j]-'a']++; 
+        for(int j = 0; j < m; j++) {
+            board[i][j+1][input[j]-'a']++;
         }
     }
     
-    vector<vector<vector<int>>> prefixSum(n+1, vector<vector<int>>(m+1, vector<int>(3, 0)));
-
-
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= m; j++) {  
-            for(int x = 0; x < 3; x++) {
+    for(int x = 0; x < 3; x++) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
                 prefixSum[i][j][x] = board[i][j][x] + 
                                     prefixSum[i-1][j][x] + 
                                     prefixSum[i][j-1][x] - 
@@ -32,14 +33,14 @@ int main() {
         }
     }
 
-    for(int i = 0; i < k; i++) {
+    while(k--) {
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
         for(int x = 0; x < 3; x++) {
             cout << prefixSum[x2][y2][x] - 
-                    prefixSum[x1-1][y2][x] -   
+                    prefixSum[x1-1][y2][x] - 
                     prefixSum[x2][y1-1][x] + 
-                    prefixSum[x1-1][y1-1][x] << ' ';  
+                    prefixSum[x1-1][y1-1][x] << ' ';
         }
         cout << '\n';
     }
