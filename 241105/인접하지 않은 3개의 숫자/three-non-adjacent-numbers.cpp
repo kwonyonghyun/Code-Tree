a2ae135c17131ed84db9380957dbd39a03b2891c
kwonyonghyun <106018526+kwonyonghyun@users.noreply.github.com>
2024-11-05 10:38:30 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241105/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.cpp
+++ 241105/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.cpp
@@ -1,24 +1,35 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 #include <climits>
+
 using namespace std;
 
-int main() {
+int main(){
     int n;
     cin>>n;
     vector<int> board(n+1);
+    vector<int> L(n+2);
+    vector<int> R(n+2);
     for(int i=0;i<n;i++){
         cin>>board[i+1];
+        L[i+1]=max(board[i+1],L[i]);
     }
+    for(int j=n;j>=1;j--){
+        R[j]=max(R[j+1],board[j]);
+    }
+    // for(int i=1;i<=n;i++){
+    //     cout<<L[i]<<' ';
+    // }
+    // cout<<endl;
+    // for(int j=1;j<=n;j++){
+    //     cout<<R[j]<<' ';
+    // }
+    cout<<endl;
     int result=INT_MIN;
-    for(int i=1;i<=n;i++){
-        for(int j=i+2;j<=n;j++){
-            for(int k=j+2;k<=n;k++){
-                result=max(result, board[i]+board[j]+board[k]);
-            }
-        }
+    for(int i=3;i<n-1;i++){
+        cout<<L[i-2]<<' '<<board[i]<< ' '<<R[i+2]<<endl;
+        result=max(result, L[i-2]+board[i]+R[i+2]);
     }
     cout<<result;
-    return 0;
 }
