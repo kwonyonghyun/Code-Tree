6e7e57c6c2e63d12f24ecb04e9c28016bbe4bb19
kwonyonghyun <106018526+kwonyonghyun@users.noreply.github.com>
2024-11-08 22:09:21 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 241108/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.cpp
+++ 241108/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.cpp
@@ -1,25 +1,25 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
 #include <algorithm>
+#include <climits>
 using namespace std;
 
-int main() {
+int main(){
     int n;
     cin>>n;
     vector<int> board(n);
     for(int i=0;i<n;i++){
         cin>>board[i];
     }
-    int result=0;
-    vector<int> results;
+    int result=INT_MIN;
+    int sum=0;
     for(int i=0;i<n;i++){
-        result+=board[i];
-        if(result<0){
-            results.push_back(result);
-            result=board[i];
+        if(sum<0){
+            sum=board[i];
+        }else{
+            sum+=board[i];
         }
+        result=max(result, sum);
     }
-    results.push_back(result);
-    cout<<*max_element(results.begin(), results.end());
-    return 0;
+    cout<<result;
 }
