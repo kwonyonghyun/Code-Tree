3183069d921716e3af05fbdce11adbfe15271ffe
kwonyonghyun <106018526+kwonyonghyun@users.noreply.github.com>
2024-12-17 17:03:09 +0900

    Title: 가장 가까운 점, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241217/가장 가까운 점/nearest-point.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+struct compare{
+    bool operator()(pair<int,int> a, pair<int,int> b){
+    return abs(a.first)+abs(a.second) > abs(b.first)+abs(b.second);
+}
+};
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    priority_queue<pair<int,int>,vector<pair<int,int>>, compare> pq;
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin>>a>>b;
+        pq.push({a,b});
+    }
+    for(int i=0;i<m;i++){
+        pair<int,int> a=pq.top();
+        pq.pop();
+        pq.push({a.first+2, a.second+2});
+    }
+    cout<<pq.top().first<<' '<<pq.top().second<<endl;
+    return 0;
+}
