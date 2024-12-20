d3b42e7f5ab137c70277e236d8d740e34ecf6894
kwonyonghyun <106018526+kwonyonghyun@users.noreply.github.com>
2024-12-20 11:22:01 +0900

    Title: 숫자 빠르게 찾기, Time: 176ms, Memory: 1MB, Status: Passed - CodeTree

--- 241220/숫자 빠르게 찾기/find-number-fast.cpp
+++ 241220/숫자 빠르게 찾기/find-number-fast.cpp
@@ -3,23 +3,39 @@
 #include <algorithm>
 using namespace std;
 #define endl '\n'
-int main() {
-    int n,m;
-    cin>>n>>m;
-    vector<int> v;
-    for(int i=0;i<n;i++){
-        int a;
-        cin>>a;
-        v.push_back(a);
-    }
-    for(int i=0;i<m;i++){
-        int a;
-        cin>>a;
-        if(find(v.begin(), v.end(), a)==v.end()){
-            cout<<-1<<endl;
+
+int binary_search(vector<int>& arr, int value){
+    int left=0;
+    int right=arr.size()-1;
+
+    while(left<=right){
+        int mid=(left+right)/2;
+        if(value<arr[mid]){
+            right=mid-1;
+        }else if(value>arr[mid]){
+            left=mid+1;
         }else{
-            cout<<find(v.begin(), v.end(), a)-v.begin()+1<<endl;
+            return mid+1;
         }
     }
+    return -1;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    
+
+    for(int i = 0; i < m; i++) {
+        int target;
+        cin >> target;
+        cout<<binary_search(v, target)<<endl;
+    }
+    
     return 0;
 }
